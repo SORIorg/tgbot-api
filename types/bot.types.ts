a9598c425484a6bd1a5bacf0dd19dc7bdd09f1c7
Habit <char.habit@gmail.com>
2023-12-15 15:29:25 +0400

    temporary fix with types

--- types/bot.types.ts
+++ types/bot.types.ts
@@ -1,8 +1,8 @@
 import TelegramBot from 'node-telegram-bot-api';
 
-export interface BotMessage extends TelegramBot.Message {}
-export interface BotMetadata extends TelegramBot.Metadata {}
-export interface BotMessageOptions extends TelegramBot.SendMessageOptions {}
+export type BotMessage = TelegramBot.Message;
+export type BotMetadata = TelegramBot.Metadata;
+export type BotMessageOptions = TelegramBot.SendMessageOptions;
 export type Constructor = new (...args: any[]) => object;
 
 export interface IClassData {
